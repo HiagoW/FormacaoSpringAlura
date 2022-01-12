f66150017bfff093a55a18cb8d9c123dfc14b40f
Hiago Petris <hiago.petris@ibm.com>
2022-01-12 09:07:47 -0300

    Autenh

--- Spring Boot/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ Spring Boot/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,6 +1,8 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +11,6 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
     // Curso_Nome indica que Curso é o relacionamento com a classe curso, e nome é o atributo dentro de Curso
     // mas dá pra usar CursoNome também
-    List<Topico> findByCurso_Nome(String nomeCurso);
+    Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 
 }
