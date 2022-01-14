852f4f21251eb21c447178b5c6e9a9b8104121c6
Hiago Petris <hiago.petris@ibm.com>
2022-01-14 10:07:36 -0300

    JWT Token, Actuator and Swagger

--- Spring Boot/forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
+++ Spring Boot/forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -32,7 +32,7 @@ public class AtualizacaoTopicoForm {
     }
 
     public Topico atualizar(Long id, TopicoRepository topicoRepository) {
-        Topico topico = topicoRepository.getById(id);
+        Topico topico = topicoRepository.findById(id).get();
         topico.setTitulo(this.titulo);
         topico.setMensagem(this.mensagem);
         return topico;
