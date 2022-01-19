3d0094f30fbf2b834beecb98650c05636a1c88c1
Hiago Petris <hiago.petris@ibm.com>
2022-01-19 08:51:26 -0300

    Tests

--- Spring Boot/forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ Spring Boot/forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -3,12 +3,13 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
-import org.h2.security.auth.AuthConfigException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@Profile(value={"prod", "test"})
 public class AutenticacaoController {
 
     @Autowired
@@ -34,7 +36,7 @@ public class AutenticacaoController {
             Authentication authentication = authManager.authenticate(dadosLogin);
             String token = tokenService.gerarToken(authentication);
             return ResponseEntity.ok(new TokenDto(token, "Bearer"));
-        } catch( AuthConfigException e){
+        } catch( AuthenticationException e){
             return ResponseEntity.badRequest().build();
         }
     }
